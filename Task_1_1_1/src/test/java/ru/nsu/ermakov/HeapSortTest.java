cc35ca2b3dfbb7dc4d802bae56745f4430ef84dd
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-09 14:00:01 +0700

    Update HeapSortTest.java

--- Task_1_1_1/src/test/java/ru/nsu/ermakov/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/ermakov/HeapSortTest.java
@@ -3,6 +3,7 @@ package ru.nsu.ermakov;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import java.util.Arrays;
 
 public class HeapSortTest {
 
@@ -38,7 +39,6 @@ public class HeapSortTest {
         int[] result = HeapSort.heapsortForTest(input);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, result);
         
-
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, input);
     }
 
@@ -131,7 +131,6 @@ public class HeapSortTest {
 
     @Test
     public void testArrayToStringEdgeCases() {
-
         String result = HeapSort.heapsort(new int[]{});
         assertEquals("[]", result);
         
@@ -151,5 +150,139 @@ public class HeapSortTest {
         
         String result = HeapSort.heapsort(input);
         assertArrayEquals(original, input); 
+    }
+
+    @Test
+    public void testLargeArray() {
+        int[] largeArray = new int[1000];
+        for (int i = 0; i < largeArray.length; i++) {
+            largeArray[i] = (int) (Math.random() * 1000);
+        }
+        
+        int[] sorted = largeArray.clone();
+        Arrays.sort(sorted);
+        
+        int[] result = HeapSort.heapsortForTest(largeArray);
+        assertArrayEquals(sorted, result);
+    }
+
+    @Test
+    public void testAlreadyMaxHeap() {
+        int[] maxHeap = {10, 8, 9, 5, 7, 6, 3};
+        int[] expected = {3, 5, 6, 7, 8, 9, 10};
+        
+        int[] result = HeapSort.heapsortForTest(maxHeap);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testAlreadyMinHeap() {
+        int[] minHeap = {1, 2, 3, 4, 5, 6, 7};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7};
+        
+        int[] result = HeapSort.heapsortForTest(minHeap);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testAlternatingNumbers() {
+        int[] input = {1, -1, 2, -2, 3, -3, 4, -4};
+        int[] expected = {-4, -3, -2, -1, 1, 2, 3, 4};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testBoundaryValues() {
+        int[] input = {Integer.MAX_VALUE, Integer.MIN_VALUE, 0, -1, 1};
+        int[] expected = {Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testPerformanceWithDuplicates() {
+        int[] input = new int[500];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i % 10;
+        }
+        
+        int[] sorted = input.clone();
+        Arrays.sort(sorted);
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(sorted, result);
+    }
+
+    @Test
+    public void testDescendingOrder() {
+        int[] input = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testSingleNegative() {
+        int[] input = {-5};
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(new int[]{-5}, result);
+        
+        String stringResult = HeapSort.heapsort(input);
+        assertEquals("[-5]", stringResult);
+    }
+
+    @Test
+    public void testAllNegative() {
+        int[] input = {-5, -10, -3, -8, -1};
+        int[] expected = {-10, -8, -5, -3, -1};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testMixedWithExtremes() {
+        int[] input = {Integer.MAX_VALUE, 0, Integer.MIN_VALUE, -1000000, 1000000};
+        int[] expected = {Integer.MIN_VALUE, -1000000, 0, 1000000, Integer.MAX_VALUE};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testStringOutputWithNegative() {
+        String result = HeapSort.heapsort(new int[]{-3, -1, 0, 2});
+        assertEquals("[-3, -1, 0, 2]", result);
+    }
+
+    @Test
+    public void testStringOutputWithSingleNegative() {
+        String result = HeapSort.heapsort(new int[]{-42});
+        assertEquals("[-42]", result);
+    }
+
+    @Test
+    public void testHeapifyProcess() {
+        int[] input = {4, 10, 3, 5, 1};
+        int[] expected = {1, 3, 4, 5, 10};
+        
+        int[] result = HeapSort.heapsortForTest(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void testMultipleCalls() {
+        int[] input1 = {5, 3, 8, 1};
+        int[] input2 = {5, 3, 8, 1};
+        
+        int[] result1 = HeapSort.heapsortForTest(input1);
+        int[] result2 = HeapSort.heapsortForTest(input2);
+        
+        assertArrayEquals(result1, result2);
+        assertArrayEquals(input1, input2);
     }
 }
