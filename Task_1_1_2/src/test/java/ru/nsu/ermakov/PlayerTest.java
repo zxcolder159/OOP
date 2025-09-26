fe36d98a4310f6a62b27fc1d7b2df850bfa8d7f1
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:58:11 +0700

    Update PlayerTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/PlayerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/PlayerTest.java
@@ -3,7 +3,6 @@ package ru.nsu.ermakov;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlayerTest {
