b111234ca8e54ef64b96171f64a5ad3864ecd3c4
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:57:12 +0700

    Update DeсkTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/DeсkTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/DeсkTest.java
@@ -7,17 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class DeckTest {
     @Test
-    void testDeckCreationAndDraw() {
+    void testDraw() {
         Deck deck = new Deck(1);
-        assertNotNull(deck.draw());
+        Card card = deck.draw();
+        assertNotNull(card);
     }
 
     @Test
-    void testDeckReshuffleWhenEmpty() {
+    void testReshuffleWhenEmpty() {
         Deck deck = new Deck(1);
-        for (int i = 0; i < 60; i++) {
+        for (int i = 0; i < 52; i++) {
             deck.draw();
         }
-        assertNotNull(deck.draw());
+        Card newCard = deck.draw();
+        assertNotNull(newCard);
+    }
+
+    @Test
+    void testDeckSizeAfterDraw() {
+        Deck deck = new Deck(1);
+        int before = deck.size();
+        deck.draw();
+        assertEquals(before - 1, deck.size());
     }
 }
