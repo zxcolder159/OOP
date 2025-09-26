75771697aea4551007fae07158798ada698520d5
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:57:41 +0700

    Update HandTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/HandTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/HandTest.java
@@ -2,7 +2,7 @@ package ru.nsu.ermakov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HandTest {
     @Test
@@ -15,19 +15,18 @@ class HandTest {
     }
 
     @Test
-    void testScoreWithAceAdjustment() {
+    void testScore() {
         Hand hand = new Hand();
-        hand.addCard(new Card(Rank.ACE, Suit.БУБНЫ));
-        hand.addCard(new Card(Rank.KING, Suit.ТРЕФЫ));
-        assertEquals(21, hand.getScore());
+        hand.addCard(new Card(Rank.ACE, Suit.ПИКИ));
+        hand.addCard(new Card(Rank.NINE, Suit.БУБНЫ));
+        assertEquals(20, hand.getScore());
     }
 
     @Test
-    void testToStringHideFirst() {
+    void testToString() {
         Hand hand = new Hand();
-        hand.addCard(new Card(Rank.TEN, Suit.ПИКИ));
-        hand.addCard(new Card(Rank.FIVE, Suit.БУБНЫ));
-        String str = hand.toString(true);
-        assertTrue(str.contains("<закрытая карта>"));
+        hand.addCard(new Card(Rank.ACE, Suit.ПИКИ));
+        String result = hand.toString(false);
+        assertEquals(true, result.contains("Туз"));
     }
 }
