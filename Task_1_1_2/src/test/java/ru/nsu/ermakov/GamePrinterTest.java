1ea3283aa5daac02c53f0844c3f09356ccf1fab9
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:43:15 +0700

    Update GamePrinterTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/GamePrinterTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/GamePrinterTest.java
@@ -1,22 +1,17 @@
 package ru.nsu.ermakov;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class GamePrinterTest {
     @Test
-    void testPrintsDoNotCrash() {
-        Player player = new Player("Игрок");
-        Dealer dealer = new Dealer("Дилер");
-        assertDoesNotThrow(() -> {
-            GamePrinter.printWelcome();
-            GamePrinter.printRoundStart(1);
-            GamePrinter.printPlayerHand(player, false);
-            GamePrinter.printDealerInitial(dealer);
-            GamePrinter.printResult(GameResult.DRAW);
-        });
+    void testPrintWelcome() {
+        assertDoesNotThrow(GamePrinter::printWelcome);
+    }
+
+    @Test
+    void testPrintRoundStart() {
+        assertDoesNotThrow(() -> GamePrinter.printRoundStart(1));
     }
 }
