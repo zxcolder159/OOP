288c50e041200e2d749f89043f8af884c52739ab
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:56:08 +0700

    Update CardTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
@@ -2,22 +2,28 @@ package ru.nsu.ermakov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CardTest {
     @Test
-    void testCardProperties() {
+    void testGetters() {
         Card card = new Card(Rank.ACE, Suit.ПИКИ);
         assertEquals(Rank.ACE, card.getRank());
         assertEquals(Suit.ПИКИ, card.getSuit());
-        assertEquals(11, card.getBaseValue());
+    }
+
+    @Test
+    void testBaseValue() {
+        Card card = new Card(Rank.KING, Suit.БУБНЫ);
+        assertEquals(10, card.getBaseValue());
     }
 
     @Test
     void testToString() {
-        Card card = new Card(Rank.KING, Suit.БУБНЫ);
-        String str = card.toString();
-        assertTrue(str.contains("Король"));
-        assertTrue(str.contains("Бубны"));
+        Card card = new Card(Rank.JACK, Suit.ТРЕФЫ);
+        String result = card.toString();
+        assertTrue(result.contains("Валет"));
+        assertTrue(result.contains("Трефы"));
     }
 }
