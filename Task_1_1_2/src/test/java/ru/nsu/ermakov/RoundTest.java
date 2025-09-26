96b6dadd10ca0090e28bc3e782cace2d01f4f0f1
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:36:26 +0700

    Update RoundTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
@@ -3,7 +3,6 @@ package ru.nsu.ermakov;
 import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class RoundTest {
