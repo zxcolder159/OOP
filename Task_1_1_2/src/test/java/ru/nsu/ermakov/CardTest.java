94e6926cca4931a2f9c0fffc14aea8d022f76078
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 16:20:41 +0700

    Update CardTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
@@ -3,6 +3,7 @@ package ru.nsu.ermakov;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CardTest {
     @Test
