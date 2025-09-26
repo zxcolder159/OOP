a621b3f770c413b329998905ecd020099b86435a
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:43:28 +0700

    Update HandTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/HandTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/HandTest.java
@@ -1,32 +1,22 @@
 package ru.nsu.ermakov;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HandTest {
     @Test
-    void testAddAndClear() {
+    void testAddCardAndClear() {
         Hand hand = new Hand();
-        hand.addCard(new Card(Rank.ACE, Suit.ПИКИ));
+        Card card = new Card(Rank.ACE, Suit.SPADES);
+        hand.addCard(card);
+
+        assertFalse(hand.getCards().isEmpty());
         assertEquals(1, hand.getCards().size());
+
         hand.clear();
-        assertEquals(0, hand.getCards().size());
-    }
-
-    @Test
-    void testGetScoreWithAce() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(Rank.ACE, Suit.ПИКИ));
-        hand.addCard(new Card(Rank.KING, Suit.БУБНЫ));
-        assertEquals(21, hand.getScore());
-    }
-
-    @Test
-    void testToString() {
-        Hand hand = new Hand();
-        hand.addCard(new Card(Rank.FIVE, Suit.ТРЕФЫ));
-        assertTrue(hand.toString(false).contains("5"));
+        assertTrue(hand.getCards().isEmpty());
     }
 }
