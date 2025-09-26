29618851345e3015c0eeba94faa3f54b4c8547e8
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 18:01:13 +0700

    Update DealerTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/DealerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/DealerTest.java
@@ -3,52 +3,65 @@ package ru.nsu.ermakov;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DealerTest {
     @Test
-    void testDealInitialCards() {
-        Dealer dealer = new Dealer("Дилер");
-        Player player = new Player("Игрок");
-        Deck deck = new Deck(1);
-
-        dealer.dealInitialCards(player, deck);
-
-        assertEquals(2, dealer.getCards().size());
-        assertEquals(2, player.getCards().size());
-    }
-
-    @Test
-    void testCompareWithPlayer() {
+    void testCompareWithPlayer_PlayerWin() {
         Dealer dealer = new Dealer("Дилер");
         Player player = new Player("Игрок");
 
+        // У игрока больше очков
+        dealer.takeCard(new Card(Rank.FIVE, Suit.БУБНЫ));
         player.takeCard(new Card(Rank.KING, Suit.ПИКИ));
-        dealer.takeCard(new Card(Rank.QUEEN, Suit.БУБНЫ));
 
         assertEquals(GameResult.PLAYER_WIN, dealer.compareWithPlayer(player));
     }
 
     @Test
-    void testDealerBust() {
+    void testCompareWithPlayer_DealerWin() {
         Dealer dealer = new Dealer("Дилер");
         Player player = new Player("Игрок");
 
-        dealer.takeCard(new Card(Rank.KING, Suit.ПИКИ));
-        dealer.takeCard(new Card(Rank.QUEEN, Suit.БУБНЫ));
+        dealer.takeCard(new Card(Rank.KING, Suit.БУБНЫ));
+        player.takeCard(new Card(Rank.FIVE, Suit.ПИКИ));
+
+        assertEquals(GameResult.DEALER_WIN, dealer.compareWithPlayer(player));
+    }
+
+    @Test
+    void testCompareWithPlayer_Draw() {
+        Dealer dealer = new Dealer("Дилер");
+        Player player = new Player("Игрок");
+
+        dealer.takeCard(new Card(Rank.TEN, Suit.БУБНЫ));
+        player.takeCard(new Card(Rank.TEN, Suit.ПИКИ));
+
+        assertEquals(GameResult.DRAW, dealer.compareWithPlayer(player));
+    }
+
+    @Test
+    void testCompareWithPlayer_PlayerBust() {
+        Dealer dealer = new Dealer("Дилер");
+        Player player = new Player("Игрок");
+
+        player.takeCard(new Card(Rank.KING, Suit.БУБНЫ));
+        player.takeCard(new Card(Rank.QUEEN, Suit.ПИКИ));
+        player.takeCard(new Card(Rank.JACK, Suit.ТРЕФЫ));
+
+        assertEquals(GameResult.DEALER_WIN, dealer.compareWithPlayer(player));
+    }
+
+    @Test
+    void testCompareWithPlayer_DealerBust() {
+        Dealer dealer = new Dealer("Дилер");
+        Player player = new Player("Игрок");
+
+        dealer.takeCard(new Card(Rank.KING, Suit.БУБНЫ));
+        dealer.takeCard(new Card(Rank.QUEEN, Suit.ПИКИ));
         dealer.takeCard(new Card(Rank.JACK, Suit.ТРЕФЫ));
 
-        assertTrue(dealer.isBusted());
+        player.takeCard(new Card(Rank.NINE, Suit.ТРЕФЫ));
+
         assertEquals(GameResult.PLAYER_WIN, dealer.compareWithPlayer(player));
-    }
-
-    @Test
-    void testDealerNotBust() {
-        Dealer dealer = new Dealer("Дилер");
-        dealer.takeCard(new Card(Rank.SEVEN, Suit.ПИКИ));
-        dealer.takeCard(new Card(Rank.NINE, Suit.БУБНЫ));
-
-        assertFalse(dealer.isBusted());
     }
 }
