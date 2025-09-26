a5644dbe1fa2329ffb75506e7ccc07604a04124d
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:45:32 +0700

    Update InputHandlerTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/InputHandlerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/InputHandlerTest.java
@@ -2,7 +2,9 @@ package ru.nsu.ermakov;
 
 import java.io.ByteArrayInputStream;
 import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
