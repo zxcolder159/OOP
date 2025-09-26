6d6802bad8399f3c337464193b235bf4c6e76e79
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:31:27 +0700

    Update GameStatsTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/GameStatsTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/GameStatsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class GameStatsTest {
     @Test
     void testAddWins() {
