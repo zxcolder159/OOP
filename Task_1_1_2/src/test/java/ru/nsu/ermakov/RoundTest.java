c5260e5512c4063268b35c1d49592d33ec634984
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:59:12 +0700

    Update RoundTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
@@ -1,20 +1,30 @@
 package ru.nsu.ermakov;
 
-import java.io.ByteArrayInputStream;
-import java.util.Scanner;
-
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RoundTest {
     @Test
-    void testPlayOneRound() {
-        ByteArrayInputStream in = new ByteArrayInputStream("\n".getBytes());
-        Scanner scanner = new Scanner(in);
+    void testPlayerBust() {
         GameStats stats = new GameStats();
+        Scanner scanner = new Scanner("1\n1\n1\n0\n");
         Round round = new Round(1, stats, scanner);
+        round.play();
 
-        assertDoesNotThrow(round::play);
+        assertEquals(1, stats.getDealerWins());
+    }
+
+    @Test
+    void testPlayerStand() {
+        GameStats stats = new GameStats();
+        Scanner scanner = new Scanner("0\n");
+        Round round = new Round(1, stats, scanner);
+        round.play();
+
+        int total = stats.getPlayerWins() + stats.getDealerWins();
+        assertEquals(true, total >= 1);
     }
 }
