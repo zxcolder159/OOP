0bf627c151770bf634308388696f7dd73a4d1e1e
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:54:00 +0700

    Update CardTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/CardTest.java
@@ -2,27 +2,22 @@ package ru.nsu.ermakov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
     @Test
-    void testGetRankAndSuit() {
+    void testCardProperties() {
         Card card = new Card(Rank.ACE, Suit.ПИКИ);
         assertEquals(Rank.ACE, card.getRank());
         assertEquals(Suit.ПИКИ, card.getSuit());
-    }
-
-    @Test
-    void testGetBaseValue() {
-        assertEquals(11, new Card(Rank.ACE, Suit.БУБНЫ).getBaseValue());
-        assertEquals(10, new Card(Rank.KING, Suit.ТРЕФЫ).getBaseValue());
-        assertEquals(7, new Card(Rank.SEVEN, Suit.ЧЕРВЫ).getBaseValue());
+        assertEquals(11, card.getBaseValue());
     }
 
     @Test
     void testToString() {
-        Card card = new Card(Rank.JACK, Suit.БУБНЫ);
-        assertTrue(card.toString().contains("Валет"));
+        Card card = new Card(Rank.KING, Suit.БУБНЫ);
+        String str = card.toString();
+        assertTrue(str.contains("Король"));
+        assertTrue(str.contains("Бубны"));
     }
 }
