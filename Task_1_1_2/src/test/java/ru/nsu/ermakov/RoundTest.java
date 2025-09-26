fc712aa92ba818fe96b91cb42384fa32834ecfe2
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:42:47 +0700

    Update RoundTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/RoundTest.java
@@ -2,13 +2,15 @@ package ru.nsu.ermakov;
 
 import java.io.ByteArrayInputStream;
 import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class RoundTest {
     @Test
     void testPlayOneRound() {
-        ByteArrayInputStream in = new ByteArrayInputStream("0\n".getBytes());
+        ByteArrayInputStream in = new ByteArrayInputStream("\n".getBytes());
         Scanner scanner = new Scanner(in);
         GameStats stats = new GameStats();
         Round round = new Round(1, stats, scanner);
