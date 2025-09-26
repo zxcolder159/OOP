fbd6df08a6e2ebe1120b206446e08979a7094fa1
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 17:31:14 +0700

    Update GamePrinterTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/GamePrinterTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/GamePrinterTest.java
@@ -3,10 +3,10 @@ package ru.nsu.ermakov;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class GamePrinterTest {
     @Test
     void testPrintsDoNotCrash() {
