64099557462966a65ab8163fa70a3a4d294a3fd2
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 18:10:37 +0700

    Update ScoreCalculatorTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/ScoreCalculatorTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/ScoreCalculatorTest.java
@@ -1,8 +1,7 @@
 package ru.nsu.ermakov;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
