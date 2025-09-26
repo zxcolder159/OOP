2eb2dce4fe838b686ffdf6656bf390b98b384163
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-09-26 16:00:42 +0700

    Update ScoreCalculatorTest.java

--- Task_1_1_2/src/test/java/ru/nsu/ermakov/ScoreCalculatorTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/ermakov/ScoreCalculatorTest.java
@@ -1,10 +1,10 @@
 package ru.nsu.ermakov;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoreCalculatorTest {
     @Test
