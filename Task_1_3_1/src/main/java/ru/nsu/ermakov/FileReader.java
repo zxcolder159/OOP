ae2eb10229dca696f8a0c70a4ab9c5ae714b171d
Alexandr Ermakov <a.ermakov@g.nsu.ru>
2025-12-01 12:47:29 +0700

    First Commit

---
+++ Task_1_3_1/src/main/java/ru/nsu/ermakov/FileReader.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Класс для чтения файла построчно с использованием кодировки UTF-8.
+ */
+public class FileReader {
+    private BufferedReader reader;
+
+    /**
+     * Конструктор для создания объекта чтения файла.
+     */
+    public FileReader(String fileName) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Читает следующую строку из файла.
+     */
+    public String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    /**
+     * Закрывает BufferedReader.
+     */
+    public void close() throws IOException {
+        reader.close();
+    }
+}
---
+++ Task_1_3_1/src/main/java/ru/nsu/ermakov/SubStringSearch.java
@@ -0,0 +1,21 @@
+/**
+ * Класс для поиска подстроки в строках текста.
+ */
+public class SubstringSearch {
+
+    /**
+     * Находит все вхождения подстроки в строке и выводит их позиции.
+     *
+     * @param text текст, в котором ищем подстроку.
+     * @param pattern подстрока для поиска.
+     * @param currentPos текущая позиция в файле для корректного вычисления абсолютной позиции.
+     */
+    public static void findSubstringInText(String text, String pattern, long currentPos) {
+        int index = 0;
+        while ((index = text.indexOf(pattern, index)) != -1) {
+            // Выводим индекс начала вхождения подстроки
+            System.out.println("Найдено на позиции: " + (currentPos + index));
+            index++;
+        }
+    }
+}
