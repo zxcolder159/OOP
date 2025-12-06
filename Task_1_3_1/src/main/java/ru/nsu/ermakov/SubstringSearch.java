b7cd0baa12c77b2df55f3ef85bd7d93135e5fe02
zxcolder159 <qwezitrakspsiblade@gmail.com>
2025-12-06 21:27:39 +0700

    Update SubstringSearch.java

--- Task_1_3_1/src/main/java/ru/nsu/ermakov/SubstringSearch.java
+++ Task_1_3_1/src/main/java/ru/nsu/ermakov/SubstringSearch.java
@@ -8,16 +8,60 @@ public class SubstringSearch {
     /**
      * Находит все вхождения подстроки в строке и выводит их позиции.
      *
-     * @param text текст, в котором ищем подстроку.
-     * @param pattern подстрока для поиска.
+     * @param text       текст, в котором ищем подстроку.
+     * @param pattern    подстрока для поиска.
      * @param currentPos текущая позиция в файле для корректного вычисления абсолютной позиции.
      */
     public static void findSubstringInText(String text, String pattern, long currentPos) {
-        int index = 0;
-        while ((index = text.indexOf(pattern, index)) != -1) {
-            // Выводим индекс начала вхождения подстроки
-            System.out.println("Найдено на позиции: " + (currentPos + index));
-            index++;
+        if (text == null || pattern == null || text.isEmpty() || pattern.isEmpty()) {
+            return;
+        }
+
+        char[] txt = text.toCharArray();
+        char[] pat = pattern.toCharArray();
+
+        int n = txt.length;
+        int m = pat.length;
+
+        if (m > n) {
+            return;
+        }
+
+        int alphabetSize = Character.MAX_VALUE + 1; // 65536
+        int[] shift = new int[alphabetSize];
+
+        for (int i = 0; i < alphabetSize; i++) {
+            shift[i] = m;
+        }
+
+
+        for (int j = 0; j < m - 1; j++) {
+            shift[pat[j]] = m - 1 - j;
+        }
+
+        int i = 0;
+
+        while (i <= n - m) {
+            int j = m - 1;
+
+            while (j >= 0 && pat[j] == txt[i + j]) {
+                j--;
+            }
+
+            if (j < 0) {
+                System.out.println("Найдено на позиции: " + (currentPos + i));
+
+                i += 1;
+            } else {
+                char badChar = txt[i + j];
+                int shiftValue = shift[badChar];
+
+                if (shiftValue <= 0) {
+                    shiftValue = 1;
+                }
+
+                i += shiftValue;
+            }
         }
     }
 }
