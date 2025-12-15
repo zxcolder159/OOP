dbe47bcedba35e2bd0fadb304419e8a953bb11a9
Alexandr Ermakov <a.ermakov@g.nsu.ru>
2025-12-15 10:33:12 +0700

    сделал визитку

--- Task_1_5_1/src/main/java/ru/nsu/ermakov/Main.java
+++ Task_1_5_1/src/main/java/ru/nsu/ermakov/Main.java
@@ -13,6 +13,10 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import static ru.nsu.ermakov.Table.Alignment.LEFT;
+import static ru.nsu.ermakov.Table.Alignment.RIGHT;
+
 public class Main {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,11 +24,19 @@ public class Main {
         String FIO = scanner.nextLine();
         Heading title = new Heading.Builder().lvl(1).value(new BoldText(FIO)).build();
         System.out.println("Введите ИНФОРМАЦИЮ О СЕБЕ(email, номер телефона в одну строчку)\n");
-        String info = scanner.nextLine();
-        BoldText infoMD = new BoldText(info);
+        ArrayList<Alignment> Alig = new ArrayList<>();
+        Alig.add(LEFT);
+        Alig.add(RIGHT);
+        ArrayList<Element> HEAD = new ArrayList<>();
+        HEAD.add(new Text ("EMAIL"));
+        HEAD.add(new Text ("Номер телефона"));
+        ArrayList<Element> tbl = new ArrayList<>();
+        tbl.add(new Text (scanner.nextLine()));
+        tbl.add(new Text (scanner.nextLine()));
+        Table table = new Table.Builder().withHeaderAndAlignments(HEAD, Alig).addRow(tbl).build();
         System.out.println("Введите количество своих достижений и затем построчно каждое из них\n");
         int cnt = scanner.nextInt();
-        ArrayList<Element> achivments = new ArrayList<>();
+
         scanner.nextLine();
         MarkDownList.Builder builder = new MarkDownList.Builder();
 
@@ -33,7 +45,7 @@ public class Main {
         }
         MarkDownList zxc = builder.build();
         Document document = new Document.Builder().addElement(title)
-                .addElement(infoMD)
+                .addElement(table)
                 .addElement(zxc)
                 .build();
         File newFile = new File("C:\\Java\\OOP\\Task_1_5_1\\src\\main\\java\\ru\\nsu\\ermakov\\vizit.MD");
