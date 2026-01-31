6364dbb3192c25a3754db6e45aec3afc10d36ea3
Alexandr Ermakov <a.ermakov@g.nsu.ru>
2026-01-31 13:31:38 +0700

    долбанный чекстайл

--- Task_2_1_1/src/main/java/ru/nsu/ermakov/IsPrime.java
+++ Task_2_1_1/src/main/java/ru/nsu/ermakov/IsPrime.java
@@ -1,13 +1,21 @@
 package ru.nsu.ermakov;
 
+/**
+ * класс со статистическим методом для проверки на простату.
+ */
 public class IsPrime {
+    /**
+     * статистический метод для проверки на простату.
+     * @param n
+     * @return
+     */
     public static boolean isPrime(long n) {
-        if (n <= 1) return false;
-        if (n <= 3) return true;
-        if (n % 2 == 0 || n % 3 == 0) return false;
+        if (n <= 1) { return false; }
+        if (n <= 3) { return true; }
+        if (n % 2 == 0 || n % 3 == 0) { return false; }
 
         for (long i = 5; i * i <= n; i += 6) {
-            if (n % i == 0 || n % (i + 2) == 0) return false;
+            if (n % i == 0 || n % (i + 2) == 0) { return false; }
         }
         return true;
     }
--- Task_2_1_1/src/test/java/ru/nsu/ermakov/StubTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/ermakov/StubTest.java
@@ -1,8 +1,15 @@
 package ru.nsu.ermakov;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Missing a Javadoc comment.
+ */
 public class StubTest {
+    /**
+     * Missing a Javadoc comment.
+     */
     @Test
     void dummyTest() {
         assertTrue(true);
