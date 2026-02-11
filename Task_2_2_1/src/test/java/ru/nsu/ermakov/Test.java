500c601a4f5060a7b0015f4f7f524d260d0912b7
zxcolder <qwezitrakspsibkade@gmail.com>
2026-02-11 16:09:30 +0700

    update: adding test

---
+++ Task_2_2_1/src/test/java/ru/nsu/ermakov/Test.java
@@ -0,0 +1,11 @@
+package ru.nsu.ermakov;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+    @Test
+    void stub() {
+        assertTrue(true);
+    }
+}
