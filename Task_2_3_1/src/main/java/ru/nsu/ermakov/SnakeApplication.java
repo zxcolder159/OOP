417638f260c418ab12b3f6c5f5f966a0c8954b7f
zxcolder <qwezitrakspsibkade@gmail.com>
2026-02-18 18:21:21 +0700

    add: gui and part of application logic

--- Task_2_3_1/src/main/java/ru/nsu/ermakov/SnakeApplication.java
+++ Task_2_3_1/src/main/java/ru/nsu/ermakov/SnakeApplication.java
@@ -1,11 +1,102 @@
 package ru.nsu.ermakov;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import ru.nsu.ermakov.game.Cell;
+import ru.nsu.ermakov.game.Game;
+import ru.nsu.ermakov.game.Point;
+import ru.nsu.ermakov.game.Snake;
+import ru.nsu.ermakov.gui.Assets;
+import javafx.animation.AnimationTimer;
+import javafx.scene.layout.StackPane;
 
 public class SnakeApplication extends Application {
-    @Override
-    public void start(Stage stage) {
+    private static final int CELL_SIZE = 40;
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 15;
+    private Cell[][] field = new Cell[20][15];
 
+
+    private Game game;
+    public void initialGame() {
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < HEIGHT; j++) {
+                field[i][j] = Cell.EMPTY;
+            }
+        }
+        field[1][0] = Cell.WALL;
+        field[2][0] = Cell.WALL;
+        field[3][0] = Cell.WALL;
+        field[4][0] = Cell.WALL;
+        Point point = new Point(10, 7);
+        game = new Game(field, point);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Assets.load();
+        initialGame();
+        Canvas canvas = new Canvas(WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setImageSmoothing(false);
+        StackPane root = new StackPane(canvas);
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Змейка");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            private long lastUpdate = 0;
+            @Override
+            public void handle(long now) {
+                if (now - lastUpdate >= 100_000_000) {
+
+
+                    draw(gc);
+                    lastUpdate = now;
+                }
+            }
+        };
+        timer.start();
+    }
+    private void draw(GraphicsContext gc) {
+        for(int x = 0; x < WIDTH; x++) {
+            for(int y = 0; y < HEIGHT; y++) {
+                if ((x + y) % 2 == 0) {
+                    gc.drawImage(Assets.bg1, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                } else {
+                    gc.drawImage(Assets.bg2, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+            }
+        }
+        for(int x = 0; x < WIDTH; x++) {
+            for(int y = 0; y < HEIGHT; y++) {
+                if(field[x][y] == Cell.WALL) {
+                    gc.drawImage(Assets.wall, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
+
+                if(field[x][y] == Cell.FOOD) {
+
+                    gc.drawImage(Assets.apple, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+                }
+            }
+        }
+        Snake snake = game.getSnake();
+        for(int i = 0; i < snake.getBody().size(); i++) {
+            Point x = snake.getBody().get(i);
+            if (x == snake.getBody().getFirst()) {
+                gc.drawImage(Assets.snakeHead, x.x() * CELL_SIZE, x.y() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+            if (x == snake.getBody().getLast()) {
+                gc.drawImage(Assets.snakeTail, x.x() * CELL_SIZE, x.y() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+
+        }
     }
 }
---
+++ Task_2_3_1/src/main/java/ru/nsu/ermakov/gui/Assets.java
@@ -0,0 +1,36 @@
+package ru.nsu.ermakov.gui;
+
+import javafx.scene.image.Image;
+
+import java.util.Objects;
+
+
+public class Assets {
+    public static Image snakeHead;
+    public static Image snakeBody;
+    public static Image snakeChange;
+    public static Image snakeTail;
+    public static Image bg1;
+    public static Image bg2;
+    public static Image apple;
+    public static Image banana;
+    public static Image wall;
+
+    public static void load() {
+        try {
+            snakeHead = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/snake_head.png")));
+            snakeBody = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/snake_body.png")));
+            snakeChange = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/snake_change.png")));
+            snakeTail = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/snake_tail.png")));
+            bg1 = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/bg1.png")));
+            bg2 = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/bg2.png")));
+            apple = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/apple.png")));
+            banana = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/banana.png")));
+            wall = new Image(Objects.requireNonNull(Assets.class.getResourceAsStream("/wall.png")));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
