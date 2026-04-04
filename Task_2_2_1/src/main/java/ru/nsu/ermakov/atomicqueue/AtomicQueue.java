d6f213fc7f204225ed392d876aa686a244e13baa
zxcolder <qwezitrakspsibkade@gmail.com>
2026-04-04 23:20:27 +0700

    i hate checkstyle

--- Task_2_2_1/src/main/java/ru/nsu/ermakov/atomicqueue/AtomicQueue.java
+++ Task_2_2_1/src/main/java/ru/nsu/ermakov/atomicqueue/AtomicQueue.java
@@ -2,14 +2,25 @@ package ru.nsu.ermakov.atomicqueue;
 
 import java.util.ArrayDeque;
 
+/**
+ * Потокобезопасная очередь на основе ArrayDeque.
+ * * @param <T> тип элементов в очереди
+ */
 public class AtomicQueue<T> {
 	private final ArrayDeque<T> deque = new ArrayDeque<>();
 	private final int capacity;
 
+	/**
+	 * Создает очередь с максимальной вместимостью (Integer.MAX_VALUE).
+	 */
 	public AtomicQueue() {
 		this(Integer.MAX_VALUE);
 	}
 
+	/**
+	 * Создает очередь с заданной вместимостью.
+	 * * @param capacity максимальная вместимость очереди
+	 */
 	public AtomicQueue(int capacity) {
 		this.capacity = capacity;
 	}
