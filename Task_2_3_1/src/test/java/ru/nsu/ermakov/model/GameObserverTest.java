d85acf4c0b7fb17f1e18d2052c54fcf2c237ba47
zxcolder <qwezitrakspsibkade@gmail.com>
2026-04-15 19:58:09 +0700

    fix: checkstyle

--- Task_2_3_1/src/test/java/ru/nsu/ermakov/model/GameObserverTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/ermakov/model/GameObserverTest.java
@@ -66,7 +66,7 @@ class GameObserverTest {
     @Test
     @DisplayName("GameObserver should receive multiple states")
     void testObserverReceivesMultipleStates() {
-        List<GameState> receivedStates = new ArrayList<>();
+
 
 
 
@@ -88,7 +88,8 @@ class GameObserverTest {
         GameState state2 = new GameState(
                 snakeBody2, Direction.RIGHT, field, false, 10, 10, false, 1, AppState.PLAYING
         );
-	    GameObserver observer = state -> receivedStates.add(state);
+        List<GameState> receivedStates = new ArrayList<>();
+        GameObserver observer = state -> receivedStates.add(state);
         observer.update(state1);
         observer.update(state2);
 
