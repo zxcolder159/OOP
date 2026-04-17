271fe8cf1143274800593150f45d0e86a9cfb278
zxcolder <qwezitrakspsibkade@gmail.com>
2026-04-17 15:49:48 +0700

    add: Student.java

---
+++ Task_2_4_1/src/main/java/ru/nsu/ermakov/student/Student.java
@@ -0,0 +1,17 @@
+package ru.nsu.ermakov.student;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+
+	private String fio;
+	private String githubNick;
+	private String repoUrl;
+
+}
