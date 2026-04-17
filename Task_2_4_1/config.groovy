d02fcc970d248a2a09198d6508e8464166758c52
zxcolder <qwezitrakspsibkade@gmail.com>
2026-04-17 17:57:52 +0700

    feat: update config.groovy

--- Task_2_4_1/config.groovy
+++ Task_2_4_1/config.groovy
@@ -1,3 +1,10 @@
+groups {
+    group {
+        name = "24216"
+        students
+    }
+}
+
 students {
     student {
         fio = "Ермаков Александр Владиславович"
@@ -8,5 +15,87 @@ students {
         fio = "Максим Ущербный Дмитриевич"
         githubNick = "vylegzhaninn"
         repoUrl = "https://github.com/vylegzhaninn/OOP"
+    }
+}
+
+tasks {
+    task {
+        name = "Task_1_1_1"
+        softDeadline = "2025-15-09"
+        hardDeadline = "2025-15-09"
+        maxScore = 1;
+
+    }
+    task {
+        name = "Task_1_1_2"
+        softDeadline = "2025-22-09"
+        hardDeadline = "2025-29-09"
+        maxScore = 2;
+    }
+    task {
+        name = "Task_1_1_3"
+        softDeadline = "2025-6-10"
+        hardDeadline = "2025-13-10"
+        maxScore = 2;
+    }
+    task {
+        name = "Task_1_2_1"
+        softDeadline = "2025-20-10"
+        hardDeadline = "2025-27-10"
+        maxScore = 2;
+    }
+    task {
+        name = "Task_1_2_2"
+        softDeadline = "2025-10-11"
+        hardDeadline = "2025-17-11"
+        maxScore = 2;
+    }
+    task {
+        name = "Task_1_3_1"
+        softDeadline = "2025-22-11"
+        hardDeadline = "2025-22-11"
+        maxScore = 2;
+    }
+    task {
+        name = "Task_1_4_1"
+        softDeadline = null
+        hardDeadline = "2025-31-11"
+        maxScore = 1;
+    }
+    task {
+        name = "Task_1_5_1"
+        softDeadline = "2025-15-12"
+        hardDeadline = "2025-27-12"
+        maxScore = 4;
+    }
+    task {
+        name = "Task_2_1_1"
+        softDeadline = null
+        hardDeadline = null
+        maxScore = 1;
+    }
+    task {
+        name = "Task_2_2_1"
+        softDeadline = null
+        hardDeadline = null
+        maxScore = 1;
+    }
+    task {
+        name = "Task_2_3_1"
+        softDeadline = null
+        hardDeadline = null
+        maxScore = 1;
+    }
+    task {
+        name = "Task_2_4_1"
+        softDeadline = null
+        hardDeadline = null
+        maxScore = 1;
+    }
+    task {
+        name = "Task_2_1_2"
+        softDeadline = null
+        hardDeadline = null
+        maxScore = 1;
     }
 }
