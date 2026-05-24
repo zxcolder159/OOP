c640e5966bed346d89f107ce677a82786f248e43
colder159 <qwezitrakspsiblade@gmail.com>
2026-05-24 17:59:13 +0700

    refactor: use Lobok's @RequiredArgsConstructor in TaskPacket class

--- Task_2_1_2/src/main/java/ru/nsu/ermakov/distributed/protocol/TaskPacket.java
+++ Task_2_1_2/src/main/java/ru/nsu/ermakov/distributed/protocol/TaskPacket.java
@@ -1,19 +1,16 @@
 package ru.nsu.ermakov.distributed.protocol;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.io.Serializable;
 
 @Getter
+@RequiredArgsConstructor
 public class TaskPacket implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final int taskId;
     private final long[] numbers;
-
-
-    public TaskPacket(int taskId, long[] numbers) {
-        this.taskId = taskId;
-        this.numbers = numbers;
-    }
 
 }
